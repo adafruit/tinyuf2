5d34d35b4a3fddae29c8dd83045654ae135615b3
hathach <thach@tinyusb.org>
2020-10-15 01:02:09 +0700

    clean up makefile

--- ports/make.mk
+++ ports/make.mk
@@ -1,6 +1,20 @@
 # ---------------------------------------
-# Common make definition for all examples
+# Common make definition for all
 # ---------------------------------------
+
+ifneq ($(lastword a b),b)
+$(error This Makefile require make 3.81 or newer)
+endif
+
+# Set TOP to be the path to get from the current directory (where make was
+# invoked) to the top of the tree. $(lastword $(MAKEFILE_LIST)) returns
+# the name of this makefile relative to where make was invoked.
+
+THIS_MAKEFILE := $(lastword $(MAKEFILE_LIST))
+TOP := $(shell realpath $(THIS_MAKEFILE))
+TOP := $(patsubst %/ports/make.mk,%,$(TOP))
+
+CURRENT_PATH := $(shell realpath --relative-to=$(TOP) `pwd`)
 
 #-------------- Select the board to build for. ------------
 BOARD_LIST = $(sort $(subst /.,,$(subst boards/,,$(wildcard boards/*/.))))
