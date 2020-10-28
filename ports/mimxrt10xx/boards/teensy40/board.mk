e84dfca1c042c475a261afcd7f3938ba28a8bc2e
hathach <thach@tinyusb.org>
2020-10-28 22:36:30 +0700

    fix teensy40 build (not tested)

--- ports/mimxrt10xx/boards/teensy40/board.mk
+++ ports/mimxrt10xx/boards/teensy40/board.mk
@@ -3,8 +3,3 @@ CFLAGS += -DCPU_MIMXRT1062DVL6A
 
 # For flash-jlink target
 JLINK_DEVICE = MIMXRT1062xxx6A
-
-# flash by using teensy_loader_cli https://github.com/PaulStoffregen/teensy_loader_cli
-# Make sure it is in your PATH 
-flash: $(BUILD)/$(BOARD)-firmware.hex
-	teensy_loader_cli --mcu=imxrt1062 -v -w $<
