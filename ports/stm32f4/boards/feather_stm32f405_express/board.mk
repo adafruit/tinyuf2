30cab4341577ef4be393feb391629df78af96114
hathach <thach@tinyusb.org>
2020-10-29 11:13:10 +0700

    add generic stm32f4 linker

--- ports/stm32f4/boards/feather_stm32f405_express/board.mk
+++ ports/stm32f4/boards/feather_stm32f405_express/board.mk
@@ -2,9 +2,6 @@ CFLAGS += \
   -DSTM32F405xx \
   -DHSE_VALUE=12000000U
 
-# All source paths should be relative to the top level.
-LD_FILE = ports/$(PORT)/boards/$(BOARD)/STM32F405RGTx_FLASH.ld
-
 SRC_S += \
 	$(ST_CMSIS)/Source/Templates/gcc/startup_stm32f405xx.s
 
