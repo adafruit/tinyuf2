60bfa51cf4851ef9d02d9ccbc84e96f99c25ac20
hathach <thach@tinyusb.org>
2020-11-13 15:34:29 +0700

    use QIO as default mode, which improve flashing speed

--- ports/esp32s2/boards/flash_hal.c
+++ ports/esp32s2/boards/flash_hal.c
@@ -30,12 +30,12 @@
 #include "board_api.h"
 
 // Debug helper, remove later
-#define PRINTF(...)           ESP_LOGI("uf2", __VA_ARGS__)
-#define PRINT_LOCATION()      ESP_LOGI("uf2", "%s: %d", __PRETTY_FUNCTION__, __LINE__)
-#define PRINT_MESS(x)         ESP_LOGI("uf2", "%s", (char*)(x))
-#define PRINT_STR(x)          ESP_LOGI("uf2", #x " = %s"   , (char*)(x) )
-#define PRINT_INT(x)          ESP_LOGI("uf2", #x " = %d"  , (int32_t) (x) )
-#define PRINT_HEX(x)          ESP_LOGI("uf2", #x " = 0x%X", (uint32_t) (x) )
+#define PRINTF(...)           ESP_LOGD("uf2", __VA_ARGS__)
+#define PRINT_LOCATION()      ESP_LOGD("uf2", "%s: %d", __PRETTY_FUNCTION__, __LINE__)
+#define PRINT_MESS(x)         ESP_LOGD("uf2", "%s", (char*)(x))
+#define PRINT_STR(x)          ESP_LOGD("uf2", #x " = %s"   , (char*)(x) )
+#define PRINT_INT(x)          ESP_LOGD("uf2", #x " = %d"  , (int32_t) (x) )
+#define PRINT_HEX(x)          ESP_LOGD("uf2", #x " = 0x%X", (uint32_t) (x) )
 
 #define PRINT_BUFFER(buf, n) \
   do {\
@@ -61,18 +61,20 @@ void board_flash_init(void)
   _part_ota0 = esp_partition_find_first(ESP_PARTITION_TYPE_APP, ESP_PARTITION_SUBTYPE_APP_OTA_0, NULL);
   assert(_part_ota0 != NULL);
 
+#if 0
+  esp_flash_t const * flash = _part_ota0->flash_chip;
   PRINT_INT(_part_ota0->type);
   PRINT_HEX(_part_ota0->subtype);
   PRINT_HEX(_part_ota0->address);
   PRINT_HEX(_part_ota0->size);
   PRINT_INT(_part_ota0->encrypted);
-
-  esp_flash_t const * flash = _part_ota0->flash_chip;
   PRINT_HEX(flash->chip_id);
   PRINT_HEX(flash->size);
 
+  // TODO use block erase to speed up writing
   uint32_t block_erase = flash->chip_drv->erase_block == NULL ? 0 : flash->chip_drv->block_erase_size;
   PRINT_HEX(block_erase);
+#endif
 }
 
 uint32_t board_flash_size(void)
@@ -89,10 +91,13 @@ void board_flash_flush(void)
 {
   if ( _fl_addr == FLASH_CACHE_INVALID_ADDR ) return;
 
-  // always write without check for contents match
-  // since reading from spi flash is also equally slow
   PRINTF("Erase and Write at 0x%08X", _fl_addr);
 
+  // always write without check for contents match
+  // since reading from spi flash is also equally slow
+  // TODO should really read back for caching
+
+  // TODO only erase if needed
   esp_partition_erase_range(_part_ota0, _fl_addr, FLASH_CACHE_SIZE);
   esp_partition_write(_part_ota0, _fl_addr, _fl_buf, FLASH_CACHE_SIZE);
 
