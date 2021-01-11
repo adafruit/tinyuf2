3e4e25ec24171fa039aaf9e7a6ed8cc78cfd8c4a
Greg Steiert <greg@steiert.net>
2021-01-11 05:02:17 +0000

    fixed self-update build for dbl-m33

--- ports/lpc55/boards/double_m33_express/board.mk
+++ ports/lpc55/boards/double_m33_express/board.mk
@@ -2,7 +2,9 @@ MCU = LPC55S69
 MCU_CORE = $(MCU)_cm33_core0
 CFLAGS += -DCPU_LPC55S69JBD100_cm33_core0
 
-SRC_C += $(TOP)/lib/sct_neopixel/sct_neopixel.c
+SRC_C += lib/sct_neopixel/sct_neopixel.c
+
+SELF_SRC_C += lib/sct_neopixel/sct_neopixel.c
 
 INC += $(TOP)/lib/sct_neopixel
 
