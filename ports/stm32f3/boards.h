609a7c1e14c5686efa694b5c5ca53503fecf6554
ReFil <31960031+ReFil@users.noreply.github.com>
2021-01-14 18:13:10 +0000

    undo flash offset

--- ports/stm32f3/boards.h
+++ ports/stm32f3/boards.h
@@ -36,7 +36,6 @@
 
 // Flash Start Address of Application
 #define BOARD_FLASH_APP_START   0x08004000
-#define BOOTLOADER_SIZE = 0x4000
 
 // Double Reset tap to enter DFU
 #define TINYUF2_DFU_DOUBLE_TAP      1
