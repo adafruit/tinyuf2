8f038d44071c05c6d63b3163f8fab1930b9df245
ReFil <31960031+ReFil@users.noreply.github.com>
2021-01-15 20:47:13 +0000

    Update boards.c

--- ports/stm32f3/boards.c
+++ ports/stm32f3/boards.c
@@ -97,19 +97,7 @@ void board_init(void)
 
 void board_dfu_init(void)
 {
-  uint32_t _timer_count = 0;
-  __HAL_RCC_USB_FORCE_RESET();
-  GPIO_InitTypeDef  GPIO_InitStruct;
-  GPIO_InitStruct.Pin = (GPIO_PIN_12);
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, 0);
-  _timer_count = 0;
-  board_timer_start(1);
-  while(_timer_count < 500) {}
-  board_timer_stop();
+
 
   __HAL_REMAPINTERRUPT_USB_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
