0b084871d12502e69398a09b8779a6401e525f9a
ReFil <31960031+ReFil@users.noreply.github.com>
2021-01-15 01:56:20 +0000

    Update boards.c

--- ports/stm32f3/boards.c
+++ ports/stm32f3/boards.c
@@ -118,7 +118,7 @@ void board_dfu_complete(void)
 bool board_app_valid(void)
 {
   //return (((*(uint32_t*)BOARD_FLASH_APP_START) - BOARD_RAM_START) <= BOARD_RAM_SIZE);
-  return true;
+  return false;
 }
 
 void board_app_jump(void)
