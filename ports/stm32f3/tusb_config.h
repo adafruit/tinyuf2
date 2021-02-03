899f8dae56ce18e3012d2aa1becf2e5d78def60d
ReFil <31960031+ReFil@users.noreply.github.com>
2021-02-03 20:29:05 +0000

    Update tusb_config.h

--- ports/stm32f3/tusb_config.h
+++ ports/stm32f3/tusb_config.h
@@ -85,7 +85,7 @@
 #define CFG_TUD_VENDOR            0
 
 // MSC Buffer size of Device Mass storage
-#define CFG_TUD_MSC_BUFSIZE    4096
+#define CFG_TUD_MSC_BUFSIZE    512
 #define CFG_TUD_HID_BUFSIZE      64
 
 #ifdef __cplusplus
