0e10d9d60523fa2beaa90e084bfe267e78792377
Henry Gabryjelski <henrygab@users.noreply.github.com>
2021-03-12 18:02:54 -0800

    WIP - additional thoughts on next steps

--- selftests/ghostfat/main.c
+++ selftests/ghostfat/main.c
@@ -20,12 +20,16 @@ int main(void)
     uint8_t singleSector[512];
     uint32_t countOfSectors_UF2 = board_flash_size() / 512;
 
+    // TODO: open disk image file for writing (binary)
     for (uint32_t i = 0; i < countOfSectors_UF2; i++) {
 
         uf2_read_block(i, singleSector);
         // TODO: append the sector to the disk image file
-        
     }
+    // TODO: close the disk image file
+    // TODO: compare the disk image file vs. expected data ... but EXCLUDE timestamps such as from directory entries!
+    // TODO: allow overriding of DOSDATE and DOSTIME in ghostfat, to allow 100% reproducible images
+
 }
 
 
