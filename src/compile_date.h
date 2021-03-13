61c2d49c5ef17f5a44ed9e87aaae68ebb4b055cb
Henry Gabryjelski <henrygab@users.noreply.github.com>
2021-03-13 14:08:58 -0800

    Allow overriding the compilation date/time

--- src/compile_date.h
+++ src/compile_date.h
@@ -26,43 +26,53 @@
 #ifndef COMPILE_DATE_H
 #define COMPILE_DATE_H
 
+#ifndef __PERSISTENT_COMPILATION_DATE__
+  #define __PERSISTENT_COMPILATION_DATE__ __DATE__
+#endif
+
+#ifndef __PERSISTENT_COMPILATION_TIME__
+  #define __PERSISTENT_COMPILATION_TIME__ __TIME__
+#endif
+
+
+
 #define __YEAR_INT__ ((( \
-  (__DATE__ [ 7u] - '0')  * 10u + \
-  (__DATE__ [ 8u] - '0')) * 10u + \
-  (__DATE__ [ 9u] - '0')) * 10u + \
-  (__DATE__ [10u] - '0'))
+  (__PERSISTENT_COMPILATION_DATE__ [ 7u] - '0')  * 10u + \
+  (__PERSISTENT_COMPILATION_DATE__ [ 8u] - '0')) * 10u + \
+  (__PERSISTENT_COMPILATION_DATE__ [ 9u] - '0')) * 10u + \
+  (__PERSISTENT_COMPILATION_DATE__ [10u] - '0'))
 
 #define __MONTH_INT__ ( \
-    (__DATE__ [2u] == 'n' && __DATE__ [1u] == 'a') ?  1u  /*Jan*/ \
-  : (__DATE__ [2u] == 'b'                        ) ?  2u  /*Feb*/ \
-  : (__DATE__ [2u] == 'r' && __DATE__ [1u] == 'a') ?  3u  /*Mar*/ \
-  : (__DATE__ [2u] == 'r'                        ) ?  4u  /*Apr*/ \
-  : (__DATE__ [2u] == 'y'                        ) ?  5u  /*May*/ \
-  : (__DATE__ [2u] == 'n'                        ) ?  6u  /*Jun*/ \
-  : (__DATE__ [2u] == 'l'                        ) ?  7u  /*Jul*/ \
-  : (__DATE__ [2u] == 'g'                        ) ?  8u  /*Aug*/ \
-  : (__DATE__ [2u] == 'p'                        ) ?  9u  /*Sep*/ \
-  : (__DATE__ [2u] == 't'                        ) ? 10u  /*Oct*/ \
-  : (__DATE__ [2u] == 'v'                        ) ? 11u  /*Nov*/ \
-  :                                                  12u  /*Dec*/ )
+    (__PERSISTENT_COMPILATION_DATE__ [2u] == 'n' && __PERSISTENT_COMPILATION_DATE__ [1u] == 'a') ?  1u  /*Jan*/ \
+  : (__PERSISTENT_COMPILATION_DATE__ [2u] == 'b'                                               ) ?  2u  /*Feb*/ \
+  : (__PERSISTENT_COMPILATION_DATE__ [2u] == 'r' && __PERSISTENT_COMPILATION_DATE__ [1u] == 'a') ?  3u  /*Mar*/ \
+  : (__PERSISTENT_COMPILATION_DATE__ [2u] == 'r'                                               ) ?  4u  /*Apr*/ \
+  : (__PERSISTENT_COMPILATION_DATE__ [2u] == 'y'                                               ) ?  5u  /*May*/ \
+  : (__PERSISTENT_COMPILATION_DATE__ [2u] == 'n'                                               ) ?  6u  /*Jun*/ \
+  : (__PERSISTENT_COMPILATION_DATE__ [2u] == 'l'                                               ) ?  7u  /*Jul*/ \
+  : (__PERSISTENT_COMPILATION_DATE__ [2u] == 'g'                                               ) ?  8u  /*Aug*/ \
+  : (__PERSISTENT_COMPILATION_DATE__ [2u] == 'p'                                               ) ?  9u  /*Sep*/ \
+  : (__PERSISTENT_COMPILATION_DATE__ [2u] == 't'                                               ) ? 10u  /*Oct*/ \
+  : (__PERSISTENT_COMPILATION_DATE__ [2u] == 'v'                                               ) ? 11u  /*Nov*/ \
+  :                                                                                                12u  /*Dec*/ )
 
 #define __DAY_INT__ ( \
-   (__DATE__ [4u] == ' ' ? 0 : __DATE__ [4u] - '0') * 10u \
- + (__DATE__ [5u] - '0')                                   )
+   (__PERSISTENT_COMPILATION_DATE__ [4u] == ' ' ? 0 : __PERSISTENT_COMPILATION_DATE__ [4u] - '0') * 10u \
+ + (__PERSISTENT_COMPILATION_DATE__ [5u] - '0')                                   )
 
 // __TIME__ expands to an eight-character string constant
 // "23:59:01", or (if cannot determine time) "??:??:??" 
 #define __HOUR_INT__ ( \
-   (__TIME__ [0u] == '?' ? 0 : __TIME__ [0u] - '0') * 10u \
- + (__TIME__ [1u] == '?' ? 0 : __TIME__ [1u] - '0')       )
+   (__PERSISTENT_COMPILATION_TIME__ [0u] == '?' ? 0 : __PERSISTENT_COMPILATION_TIME__ [0u] - '0') * 10u \
+ + (__PERSISTENT_COMPILATION_TIME__ [1u] == '?' ? 0 : __PERSISTENT_COMPILATION_TIME__ [1u] - '0')       )
 
 #define __MINUTE_INT__ ( \
-   (__TIME__ [3u] == '?' ? 0 : __TIME__ [3u] - '0') * 10u \
- + (__TIME__ [4u] == '?' ? 0 : __TIME__ [4u] - '0')       )
+   (__PERSISTENT_COMPILATION_TIME__ [3u] == '?' ? 0 : __PERSISTENT_COMPILATION_TIME__ [3u] - '0') * 10u \
+ + (__PERSISTENT_COMPILATION_TIME__ [4u] == '?' ? 0 : __PERSISTENT_COMPILATION_TIME__ [4u] - '0')       )
 
 #define __SECONDS_INT__ ( \
-   (__TIME__ [6u] == '?' ? 0 : __TIME__ [6u] - '0') * 10u \
- + (__TIME__ [7u] == '?' ? 0 : __TIME__ [7u] - '0')       )
+   (__PERSISTENT_COMPILATION_TIME__ [6u] == '?' ? 0 : __PERSISTENT_COMPILATION_TIME__ [6u] - '0') * 10u \
+ + (__PERSISTENT_COMPILATION_TIME__ [7u] == '?' ? 0 : __PERSISTENT_COMPILATION_TIME__ [7u] - '0')       )
 
 
 #define __DOSDATE__ ( \
