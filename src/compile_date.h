f329fd315fff94ba2fd73a917002a86b5d0772f5
Henry Gabryjelski <henrygab@users.noreply.github.com>
2021-03-14 14:29:12 -0700

    Avoid double underscores

--- src/compile_date.h
+++ src/compile_date.h
@@ -37,15 +37,13 @@
   #define REPRODUCIBLE_TIME __TIME__
 #endif
 
-
-
-#define __YEAR_INT__ ((( \
+#define COMPILE_YEAR_INT ((( \
   (COMPILE_DATE [ 7u] - '0')  * 10u + \
   (COMPILE_DATE [ 8u] - '0')) * 10u + \
   (COMPILE_DATE [ 9u] - '0')) * 10u + \
   (COMPILE_DATE [10u] - '0'))
 
-#define __MONTH_INT__ ( \
+#define COMPILE_MONTH_INT  ( \
     (COMPILE_DATE [2u] == 'n' && COMPILE_DATE [1u] == 'a') ?  1u  /*Jan*/ \
   : (COMPILE_DATE [2u] == 'b'                            ) ?  2u  /*Feb*/ \
   : (COMPILE_DATE [2u] == 'r' && COMPILE_DATE [1u] == 'a') ?  3u  /*Mar*/ \
@@ -59,35 +57,35 @@
   : (COMPILE_DATE [2u] == 'v'                            ) ? 11u  /*Nov*/ \
   :                                                          12u  /*Dec*/ )
 
-#define __DAY_INT__ ( \
+#define COMPILE_DAY_INT ( \
    (COMPILE_DATE [4u] == ' ' ? 0 : COMPILE_DATE [4u] - '0') * 10u + \
    (COMPILE_DATE [5u] - '0')                                             \
    )
 
 // __TIME__ expands to an eight-character string constant
 // "23:59:01", or (if cannot determine time) "??:??:??" 
-#define __HOUR_INT__ ( \
+#define COMPILE_HOUR_INT ( \
    (COMPILE_TIME [0u] == '?' ? 0 : COMPILE_TIME [0u] - '0') * 10u \
  + (COMPILE_TIME [1u] == '?' ? 0 : COMPILE_TIME [1u] - '0')       )
 
-#define __MINUTE_INT__ ( \
+#define COMPILE_MINUTE_INT ( \
    (COMPILE_TIME [3u] == '?' ? 0 : COMPILE_TIME [3u] - '0') * 10u \
  + (COMPILE_TIME [4u] == '?' ? 0 : COMPILE_TIME [4u] - '0')       )
 
-#define __SECONDS_INT__ ( \
+#define COMPILE_SECONDS_INT ( \
    (COMPILE_TIME [6u] == '?' ? 0 : COMPILE_TIME [6u] - '0') * 10u \
  + (COMPILE_TIME [7u] == '?' ? 0 : COMPILE_TIME [7u] - '0')       )
 
 
 #define COMPILE_DOS_DATE ( \
-	((__YEAR_INT__  - 1980u) << 9u) | \
-	( __MONTH_INT__          << 5u) | \
-	( __DAY_INT__            << 0u) )
+	((COMPILE_YEAR_INT  - 1980u) << 9u) | \
+	( COMPILE_MONTH_INT          << 5u) | \
+	( COMPILE_DAY_INT            << 0u) )
 
 #define COMPILE_DOS_TIME ( \
-	( __HOUR_INT__    << 11u) | \
-	( __MINUTE_INT__  <<  5u) | \
-	( __SECONDS_INT__ <<  0u) )
+	( COMPILE_HOUR_INT    << 11u) | \
+	( COMPILE_MINUTE_INT  <<  5u) | \
+	( COMPILE_SECONDS_INT <<  0u) )
 
 #endif // COMPILE_DATE_H
 
