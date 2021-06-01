e8b569d0c15969bce7627247b92af15a0dda1dc2
charkster <31560568+charkster@users.noreply.github.com>
2021-05-31 23:35:45 -0700

    Add files via upload

---
+++ ports/stm32f4/boards/stm32f411ce_blackpill/board.mk
@@ -0,0 +1,15 @@
+CFLAGS += \
+  -DSTM32F411xE \
+  -DHSE_VALUE=25000000U
+
+SRC_S += \
+	$(ST_CMSIS)/Source/Templates/gcc/startup_stm32f411xe.s
+
+# For flash-jlink target
+JLINK_DEVICE = stm32f411ce
+
+flash: flash-jlink
+erase: erase-jlink
+
+#flash: flash-stlink
+#erase: erase-stlink
