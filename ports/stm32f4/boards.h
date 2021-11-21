f60d53eba256e1d946f250b5cf7d4bc70417278e
Oliver Joos <oliver.joos@hispeed.ch>
2021-11-21 12:57:39 +0100

    allow for a custom application start vector on stm32
    To set a custom application start vector define in your "board.h":
      #define BOARD_FLASH_APP_START 0x08020000
    
    The default start vectors are still 0x08004000 on stm32f3 and 0x08010000 on stm32f4. Other common start vectors on stm32f4 are 0x08008000 and 0x08020000.

--- ports/stm32f4/boards.h
+++ ports/stm32f4/boards.h
@@ -35,7 +35,9 @@
 #include "board.h"
 
 // Flash Start Address of Application
+#ifndef BOARD_FLASH_APP_START
 #define BOARD_FLASH_APP_START   0x08010000
+#endif
 
 // Double Reset tap to enter DFU
 #define TINYUF2_DFU_DOUBLE_TAP  1
