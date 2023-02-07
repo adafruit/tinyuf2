ac93dba4fa79ac90fe9fa16c0bb0e1a47313906c
hathach <thach@tinyusb.org>
2023-02-07 20:49:38 +0700

    clean up

--- ports/mimxrt10xx/boards/metro_m7_1011/flash_config.c
+++ ports/mimxrt10xx/boards/metro_m7_1011/flash_config.c
@@ -90,7 +90,7 @@ const flexspi_nor_config_t qspiflash_config = {
                      FLEXSPI_LUT_SEQ(DUMMY_SDR, FLEXSPI_4PAD, 6 /* 6 dummy cycles, 2 for M7-0 and 4 dummy */,
                                      READ_SDR,  FLEXSPI_4PAD, 0x04),
             // Single fast read version, good for debugging.
-            // FLEXSPI_LUT_SEQ(CMD_SDR,   FLEXSPI_1Pcommand codemand to send */,
+            // FLEXSPI_LUT_SEQ(CMD_SDR,   FLEXSPI_1PAD, 0x0B /* command code */,
             //                 RADDR_SDR, FLEXSPI_1PAD, 24  /* bits to transmit */),
             // FLEXSPI_LUT_SEQ(DUMMY_SDR, FLEXSPI_1PAD, 8 /* 8 dummy clocks */,
             //                 READ_SDR,  FLEXSPI_1PAD, 0x04),
