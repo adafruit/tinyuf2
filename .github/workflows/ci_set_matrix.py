6b89cbc101fcb5aff6846191ee7b07d209a621a0
hathach <thach@tinyusb.org>
2024-05-31 10:07:44 +0700

    sorted ci port/board

--- .github/workflows/ci_set_matrix.py
+++ .github/workflows/ci_set_matrix.py
@@ -8,13 +8,13 @@ TOP = Path(__file__).parent.parent.parent.resolve()
 def set_matrix_json():
     ports_dir = Path(TOP / 'ports')
     matrix = {}
-    for p in ports_dir.iterdir():
+    for p in sorted(ports_dir.iterdir()):
         if p.is_dir() and p.name != 'template_port':
             matrix[p.name] = {}
             boards_dir = ports_dir / p / 'boards'
             if boards_dir.exists():
                 matrix[p.name]['board'] = []
-                for b in boards_dir.iterdir():
+                for b in sorted(boards_dir.iterdir()):
                     if b.is_dir():
                         matrix[p.name]['board'].append(b.name)
     print(json.dumps(matrix))
