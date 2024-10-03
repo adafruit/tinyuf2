1268fb2147adfa805db57dea8d1296acc3eb922b
Bill Sideris <bill88t@feline.gr>
2024-10-03 18:20:38 +0300

    Update copyheader for the board

--- ports/espressif/boards/waveshare_esp32_s3_zero/board.h
+++ ports/espressif/boards/waveshare_esp32_s3_zero/board.h
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2020 Ha Thach (tinyusb.org) for Adafruit Industries
+ * Copyright (c) 2024 Bill Sideris, independently providing these changes.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
