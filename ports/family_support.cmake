23ccf11b83159b1c5a88026b9c4a092c00a09c20
hathach <thach@tinyusb.org>
2025-05-22 22:40:45 +0700

    update cmake

--- ports/family_support.cmake
+++ ports/family_support.cmake
@@ -1,4 +1,4 @@
-include_guard()
+include_guard(GLOBAL)
 
 include(CMakePrintHelpers)
 find_package(Python COMPONENTS Interpreter)
@@ -6,13 +6,36 @@ find_package(Git REQUIRED)
 
 # TOP is path to root directory
 set(TOP ${CMAKE_CURRENT_LIST_DIR}/..)
-#get_filename_component(TOP "${TOP}" REALPATH)
+get_filename_component(TOP ${TOP} ABSOLUTE)
 
-# Default to gcc
+set(UF2CONV_PY ${TOP}/lib/uf2/utils/uf2conv.py)
+
+#-------------------------------------------------------------
+# Toolchain
+# Can be changed via -DTOOLCHAIN=gcc|iar or -DCMAKE_C_COMPILER=
+#-------------------------------------------------------------
+# Detect toolchain based on CMAKE_C_COMPILER
+if (DEFINED CMAKE_C_COMPILER)
+  string(FIND ${CMAKE_C_COMPILER} "iccarm" IS_IAR)
+  string(FIND ${CMAKE_C_COMPILER} "clang" IS_CLANG)
+  string(FIND ${CMAKE_C_COMPILER} "gcc" IS_GCC)
+
+  if (NOT IS_IAR EQUAL -1)
+    set(TOOLCHAIN iar)
+  elseif (NOT IS_CLANG EQUAL -1)
+    set(TOOLCHAIN clang)
+  elseif (NOT IS_GCC EQUAL -1)
+    set(TOOLCHAIN gcc)
+  endif ()
+endif ()
+
 if (NOT DEFINED TOOLCHAIN)
   set(TOOLCHAIN gcc)
 endif ()
 
+#-------------------------------------------------------------
+# FAMILY and BOARD
+#-------------------------------------------------------------
 if (NOT BOARD)
   message(FATAL_ERROR "BOARD not specified")
 endif ()
@@ -33,8 +56,6 @@ if (NOT DEFINED FAMILY)
   list(GET BOARD_PATH 0 FAMILY)
 endif ()
 
-set(UF2CONV_PY ${TOP}/lib/uf2/utils/uf2conv.py)
-
 # enable LTO if supported
 include(CheckIPOSupported)
 check_ipo_supported(RESULT IPO_SUPPORTED)
@@ -50,29 +71,34 @@ function(family_add_bin_hex TARGET)
   # placeholder, will be override by family specific
 endfunction()
 
-function(family_add_default_example_warnings TARGET)
-#  target_compile_options(${TARGET} PUBLIC
-#    -Wall
-#    -Wextra
-#    -Werror
-#    -Wfatal-errors
-#    -Wdouble-promotion
-#    -Wfloat-equal
-#    -Wshadow
-#    -Wwrite-strings
-#    -Wsign-compare
-#    -Wmissing-format-attribute
-#    -Wunreachable-code
-#    -Wcast-align
-#    -Wcast-qual
-#    -Wnull-dereference
-#    -Wuninitialized
-#    -Wunused
-#    -Wredundant-decls
-#    #-Wstrict-prototypes
-#    #-Werror-implicit-function-declaration
-#    #-Wundef
-#    )
+function(family_add_default_warnings TARGET)
+  target_compile_options(${TARGET} PUBLIC
+    -Wall
+    -Wextra
+    #-Werror
+    -Wfatal-errors
+    -Wdouble-promotion
+    -Wstrict-prototypes
+    -Wstrict-overflow
+    -Werror-implicit-function-declaration
+    -Wfloat-equal
+    -Wundef
+    -Wshadow
+    -Wwrite-strings
+    -Wsign-compare
+    -Wmissing-format-attribute
+    -Wunreachable-code
+    -Wcast-align
+    -Wcast-function-type
+    -Wcast-qual
+    -Wnull-dereference
+    -Wuninitialized
+    -Wunused
+    -Wunused-function
+    -Wreturn-type
+    -Wredundant-decls
+    -Wmissing-prototypes
+    )
 
   if (CMAKE_C_COMPILER_ID STREQUAL "GNU")
     if (CMAKE_C_COMPILER_VERSION VERSION_GREATER_EQUAL 12.0)
@@ -98,19 +124,18 @@ endfunction()
 
 
 function(family_configure_common TARGET)
-  # run size after build
-  add_custom_command(TARGET ${TARGET} POST_BUILD
-    COMMAND ${CMAKE_SIZE} $<TARGET_FILE:${TARGET}>
-    )
+  # Add BOARD_${BOARD} define
+  string(TOUPPER ${BOARD} BOARD_UPPER)
+  string(REPLACE "-" "_" BOARD_UPPER ${BOARD_UPPER})
+  target_compile_definitions(${TARGET} PUBLIC
+    BOARD_${BOARD_UPPER}
+  )
 
-  # add hex, bin and uf2 targets
-  family_add_bin_hex(${TARGET})
-
-  # Add warnings flags
-
-  # Generate map file
-  target_link_options(${TARGET} PUBLIC "LINKER:-Map=$<TARGET_FILE:${TARGET}>.map")
-  #family_add_linkermap(${TARGET})
+  # compile define from command line
+  if(DEFINED CFLAGS_CLI)
+    separate_arguments(CFLAGS_CLI)
+    target_compile_options(${TARGET} PUBLIC ${CFLAGS_CLI})
+  endif()
 
   # executable target linked with board target
   family_add_board_target(board_${BOARD})
@@ -127,76 +152,40 @@ function(family_configure_common TARGET)
 
     # Add segger rtt to example
     if(LOGGER STREQUAL "RTT" OR LOGGER STREQUAL "rtt")
-      if (NOT TARGET segger_rtt)
-        add_library(segger_rtt STATIC ${TOP}/lib/SEGGER_RTT/RTT/SEGGER_RTT.c)
-        target_include_directories(segger_rtt PUBLIC ${TOP}/lib/SEGGER_RTT/RTT)
-      endif()
-      target_link_libraries(${TARGET} PUBLIC segger_rtt)
+      target_sources(${TARGET} PUBLIC ${TOP}/lib/SEGGER_RTT/RTT/SEGGER_RTT.c)
+      target_include_directories(${TARGET}  PUBLIC ${TOP}/lib/SEGGER_RTT/RTT)
+#      target_compile_definitions(${TARGET}  PUBLIC SEGGER_RTT_MODE_DEFAULT=SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL)
     endif ()
   endif ()
+
+  family_add_default_warnings(${TARGET})
+  target_link_options(${TARGET} PUBLIC "LINKER:-Map=$<TARGET_FILE:${TARGET}>.map")
+
+  # run size after build
+  add_custom_command(TARGET ${TARGET} POST_BUILD
+    COMMAND ${CMAKE_SIZE} $<TARGET_FILE:${TARGET}>
+    )
+
+  # add hex, bin and uf2 targets
+  family_add_bin_hex(${TARGET})
+
 endfunction()
 
 
 # Add tinyusb to example
-function(family_add_tinyusb TARGET OPT_MCU RTOS)
-  # tinyusb target is built for each example since it depends on example's tusb_config.h
-  set(TINYUSB_TARGET_PREFIX ${TARGET}-)
-  add_library(${TARGET}-tinyusb_config INTERFACE)
-
-  # path to tusb_config.h
-  target_include_directories(${TARGET}-tinyusb_config INTERFACE ${CMAKE_CURRENT_SOURCE_DIR})
-  target_compile_definitions(${TARGET}-tinyusb_config INTERFACE CFG_TUSB_MCU=${OPT_MCU})
-
-  if (DEFINED LOG)
-    target_compile_definitions(${TARGET}-tinyusb_config INTERFACE CFG_TUSB_DEBUG=${LOG})
-    if (LOG STREQUAL "4")
-      # no inline for debug level 4
-      target_compile_definitions(${TARGET}-tinyusb_config INTERFACE TU_ATTR_ALWAYS_INLINE=)
-    endif ()
-  endif()
-
-  if (RTOS STREQUAL "freertos")
-    target_compile_definitions(${TARGET}-tinyusb_config INTERFACE CFG_TUSB_OS=OPT_OS_FREERTOS)
-  endif ()
-
+function(family_add_tinyusb TARGET OPT_MCU)
   # tinyusb's CMakeList.txt
   add_subdirectory(${TOP}/lib/tinyusb/src ${CMAKE_CURRENT_BINARY_DIR}/tinyusb)
 
-  if (RTOS STREQUAL "freertos")
-    target_link_libraries(${TARGET}-tinyusb PUBLIC freertos_kernel)
-  endif ()
-
-  # tinyusb depends on board target for low level mcu header and function
-  target_link_libraries(${TARGET}-tinyusb PUBLIC board_${BOARD})
-  target_link_libraries(${TARGET} PUBLIC ${TARGET}-tinyusb)
-endfunction()
-
-function(family_add_uf2version TARGET DEPS_REPO)
-  execute_process(COMMAND ${GIT_EXECUTABLE} describe --dirty --always --tags OUTPUT_VARIABLE GIT_VERSION)
-  string(STRIP ${GIT_VERSION} GIT_VERSION)
-#  string(REPLACE ${TOP}/ "" DEPS_REPO "${DEPS_REPO}")
-#  foreach (DEP ${DEPS_REPO})
-#    execute_process(COMMAND ${GIT_EXECUTABLE} -C ${TOP} submodule status ${DEP}
-#      OUTPUT_VARIABLE DEP_VERSION
-#      )
-#    string(STRIP ${DEP_VERSION} DEP_VERSION)
-#    string(FIND "${DEP_VERSION}" " " SPACE_POS)
-#    string(SUBSTRING "${DEP_VERSION}" ${SPACE_POS} -1 DEP_VERSION)
-#    string(STRIP ${DEP_VERSION} DEP_VERSION)
-#
-#    set(GIT_SUBMODULE_VERSIONS "${GIT_SUBMODULE_VERSIONS} ${DEP_VERSION}")
-#  endforeach ()
-#
-#  string(STRIP ${GIT_SUBMODULE_VERSIONS} GIT_SUBMODULE_VERSIONS)
-#  string(REPLACE lib/ "" GIT_SUBMODULE_VERSIONS ${GIT_SUBMODULE_VERSIONS})
-
-  cmake_print_variables(GIT_VERSION)
-  cmake_print_variables(GIT_SUBMODULE_VERSIONS)
-
-  target_compile_definitions(${TARGET} PUBLIC
-    UF2_VERSION_BASE="${GIT_VERSION}"
-    UF2_VERSION="${GIT_VERSION} - ${GIT_SUBMODULE_VERSIONS}"
-    )
+  # Add TinyUSB sources, include and common define
+  tinyusb_target_add(${TARGET})
+  target_compile_definitions(${TARGET} PUBLIC CFG_TUSB_MCU=${OPT_MCU})
+  if (DEFINED LOG)
+    target_compile_definitions(${TARGET} PUBLIC CFG_TUSB_DEBUG=${LOG})
+    if (LOG STREQUAL "4") # no inline for debug level 4
+      target_compile_definitions(${TARGET} PUBLIC TU_ATTR_ALWAYS_INLINE=)
+    endif ()
+  endif()
 endfunction()
 
 function(family_configure_tinyuf2 TARGET OPT_MCU)
@@ -205,8 +194,16 @@ function(family_configure_tinyuf2 TARGET OPT_MCU)
   include(${TOP}/src/tinyuf2.cmake)
   add_tinyuf2(${TARGET})
 
-  family_add_uf2version(${TARGET} "${FAMILY_SUBMODULE_DEPS}")
-  family_add_tinyusb(${TARGET} ${OPT_MCU} none)
+  family_add_tinyusb(${TARGET} ${OPT_MCU})
+
+  # Add uf2 version
+  execute_process(COMMAND ${GIT_EXECUTABLE} describe --dirty --always --tags OUTPUT_VARIABLE GIT_VERSION)
+  string(STRIP ${GIT_VERSION} GIT_VERSION)
+  cmake_print_variables(GIT_VERSION)
+  target_compile_definitions(${TARGET} PUBLIC
+    UF2_VERSION_BASE="${GIT_VERSION}"
+    UF2_VERSION="${GIT_VERSION}"
+    )
 endfunction()
 
 
@@ -236,12 +233,6 @@ function(family_add_uf2 TARGET FAMILY_ID)
   add_custom_command(TARGET ${TARGET} POST_BUILD
     COMMAND echo ${Python_EXECUTABLE} ${UF2CONV_PY} -f ${FAMILY_ID} ${ADDR_OPT} -c -o $<TARGET_FILE_DIR:${TARGET}>/${TARGET}.uf2 ${BIN_FILE}
     COMMAND ${Python_EXECUTABLE} ${UF2CONV_PY} -f ${FAMILY_ID} ${ADDR_OPT} -c -o $<TARGET_FILE_DIR:${TARGET}>/${TARGET}.uf2 ${BIN_FILE}
-    VERBATIM)
-endfunction()
-
-function(family_add_linkermap TARGET)
-  add_custom_command(TARGET ${TARGET} POST_BUILD
-    COMMAND linkermap -v $<TARGET_FILE:${TARGET}>.map
     VERBATIM)
 endfunction()
 
