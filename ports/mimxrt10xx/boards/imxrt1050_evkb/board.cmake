dc036b3aa8b1fdd62412e5a7ff2b2f2c8f8380fd
hathach <thach@tinyusb.org>
2025-12-29 13:06:00 +0700

    refactor pin_mux to use  mcux pin config tool

--- ports/mimxrt10xx/boards/imxrt1050_evkb/board.cmake
+++ ports/mimxrt10xx/boards/imxrt1050_evkb/board.cmake
@@ -4,10 +4,6 @@ set(JLINK_DEVICE MIMXRT1052xxxxB)
 set(PYOCD_TARGET mimxrt1052)
 
 function(update_board TARGET)
-  target_sources(${TARGET} PRIVATE
-    ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/clock_config.c
-    ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/flash_config.c
-    )
   target_compile_definitions(${TARGET} PUBLIC
     CPU_MIMXRT1052DVL6B
     )
